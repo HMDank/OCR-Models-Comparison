1ab7539abf888a8db8d61785716eb28d66c3afdc
HMDank <hminhdang05@gmail.com>
2024-12-25 15:30:39 +0100

    feat: add pre-selected images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from utils import process_image
+import os
+from utils import process_image, load_selected_images
 
 st.set_page_config(
     page_title="Testing OCR Models",
@@ -10,7 +11,11 @@ st.set_page_config(
 
 st.title('OCR Test', anchor=False)
 
-results_df = pd.DataFrame(columns=['Preview Image', 'Result', 'Processing Time'])
+@st.cache_data
+def get_selected_images():
+    return load_selected_images()
+
+results_df = pd.DataFrame(columns=['Model', 'Preview Image', 'Result', 'Processing Time'])
 if 'links' not in st.session_state:
     st.session_state["links"] = []
 
@@ -18,8 +23,6 @@ column1, column2, column3 = st.columns(3)
 with column1:
     uploaded_images = st.file_uploader("Input image(s)", accept_multiple_files=True, type=['png', 'jpg'])
 with column2:
-    st.header("AND/OR", anchor=False)
-with column3:
     link = st.text_input("URL of Image")
     subcol1, subcol2 = st.columns(2)
     with subcol1:
@@ -30,18 +33,24 @@ with column3:
             st.session_state["links"] = []
     if st.session_state["links"]:
         st.write(st.session_state["links"])
+with column3:
+    checked = st.checkbox("Use selected Data (Pictures taken by Dank himself :>)")
+    image_list = get_selected_images() if checked else []
 choices = st.multiselect('Select Models:', ['easyocr'])
-if (uploaded_images or st.session_state["links"]) and choices :
+if (uploaded_images or st.session_state["links"] or checked) and choices :
     if st.button('Start Analysis', use_container_width=True):
         images_df = process_image(choices, uploaded_images)
         links_df = process_image(choices, st.session_state["links"])
-        
-        results_df = pd.concat([images_df, links_df], ignore_index=True)
+        selected_df = process_image(choices, image_list)
+        results_df = pd.concat([images_df, links_df, selected_df], ignore_index=True)
         
         st.dataframe(
             results_df,
-            column_order=("Image", "Result", "Processing Time"  ),
+            column_order=("Model", "Image", "Result", "Processing Time"  ),
             column_config={
+                "Model": st.column_config.Column(
+                    "Model", width='small'
+                ),
                 "Image": st.column_config.ImageColumn(
                     "Preview Image", width='small'
                 ),
--- utils.py
+++ utils.py
@@ -10,8 +10,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+from PIL.JpegImagePlugin import JpegImageFile
 from io import BytesIO
 import requests
+from urllib.parse import urlparse
+
+def load_selected_images():
+    folder_path = "selected_images"
+    image_extensions = (".jpg", ".jpeg", ".png")
+    image_list = [
+        os.path.join(folder_path, file)
+        for file in os.listdir(folder_path)
+        if file.lower().endswith(image_extensions)
+    ]
+
+
+    return image_list
+
 
 def upload_image_to_cloudinary(pil_image):
     # Configure Cloudinary
@@ -43,22 +58,34 @@ def process_image(method_list, inputs):
         return results_df
     for method in method_list:
         start_time = time.time()
+        
         if method == 'easyocr':
             reader = easyocr.Reader(['de', 'en'])
+            def is_url(string):
+                if isinstance(input, str):
+                    parsed = urlparse(string)
+                    return bool(parsed.scheme and parsed.netloc)
             for input in inputs:
-                if isinstance(input, st.runtime.uploaded_file_manager.UploadedFile):
-                    input_image = np.array(Image.open(input))
-                    image = Image.open(input)
-                elif isinstance(input, str):
+                if is_url(input):
                     response = requests.get(input)
                     input_image = image = Image.open(BytesIO(response.content))
+                elif isinstance(input, str):
+                    input_image = input
+                    image = Image.open(input)
+                elif isinstance(input, st.runtime.uploaded_file_manager.UploadedFile):
+                    input_image = np.array(Image.open(input))
+                    image = Image.open(input)
+
                 final_image = upload_image_to_cloudinary(image)
                 result = reader.readtext(input_image, detail=0, paragraph=True)
                 end_time = time.time()
                 temp_df = pd.DataFrame([{
+                    'Model': method,
                     'Image': final_image,
                     'Result': result,
                     'Processing Time': round(end_time - start_time, 3)
                 }])
                 results_df = pd.concat([results_df, temp_df], ignore_index=True)
+        
+        # if method == 
     return results_df
